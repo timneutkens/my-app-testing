1330e735d5dcca5c2dbbff97956c548e5eae31ea
Tim Neutkens <timneutkens@me.com>
2021-03-02 15:20:50 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,3 @@
-const Error = ({ statusCode }) => <>test123</>;
-Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
-  return { statusCode };
-};
-export default Error;
+export default function Hello() {
+  return <div>test</div>
+}
