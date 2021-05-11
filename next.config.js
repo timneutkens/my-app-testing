2f19a932f40ba2c482be39780dec759a1195f80a
Tim Neutkens <timneutkens@me.com>
2021-05-11 11:38:26 +0200

    Test 23590

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  future: {
-    webpack5: true,
+  images: {
+    domains: ['cdn.sanity.io', 'pbs.twimg.com'],
   },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,14 @@
-export default function Hello({ props }) {
-  return <h1>Test app</h1>;
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Image
+        src="https://pbs.twimg.com/media/E00DyJhXoAYlBYL?format=jpg&name=4096x4096"
+        width={2000}
+        height={2000}
+      />
+    </>
+  );
 }
