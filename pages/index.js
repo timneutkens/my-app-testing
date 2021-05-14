da6856f8a3a1d79bd2e95fa74f19e1243ee64255
Tim Neutkens <timneutkens@me.com>
2021-05-14 11:29:54 +0200

    Basic test

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <>
-      <h1>Hello World</h1>
-      <Image
-        src="https://pbs.twimg.com/media/E00DyJhXoAYlBYL?format=jpg&name=4096x4096"
-        width={2000}
-        height={2000}
-      />
-    </>
-  );
+  return <h1>Hello World</h1>
 }
